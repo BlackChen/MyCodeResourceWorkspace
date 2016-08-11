ad602279a22e98519a77f07eaca7b2a5706cb111
BlackChen <1414925741@qq.com>
2016-08-11 13:23:34 +0800

    测试bot

---
+++ Bot_Test/Bot_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bot_Test
+//
+//  Created by 陈黔 on 16/8/11.
+//  Copyright © 2016年 BlackChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bot_Test/Bot_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bot_Test
+//
+//  Created by 陈黔 on 16/8/11.
+//  Copyright © 2016年 BlackChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
